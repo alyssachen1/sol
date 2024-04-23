879780cb3dd99df46db6a5486f408d9591c05d72
Alyssa Chen <alyssa_chen1@berkeley.edu>
2024-04-23 15:43:23 -0700

    updated

--- contracts/EthPool.sol
+++ contracts/EthPool.sol
@@ -3,117 +3,83 @@ pragma solidity ^0.8.15;
 // import "hardhat/console.sol";
 import "./SafeMath.sol";
 
-
 contract EthPool {
 
-    struct Deposit {
-        uint256 amount;
-        uint256 rewardEligibilityCounter; // track eligibility of rewards
-    }
+    address owner;
+    uint public poolBalance;
+    uint public reward;
+    uint countDeposit;
+    uint countWithdraw;
 
-    using SafeMath for uint256;
-    address public team;
-    uint256 public totalPool;
-    mapping(address => Deposit[]) public userDeposits;
-    uint256 public accRewardPerEth; 
-    uint public rewardCounter = 0;
-    
-    event RewardAdded(address indexed totalPool, string message);
-    event DepositMade(address indexed depositer, uint256 amount);
-    event Withdrawn(address indexed depositer, uint256 amount);
+    mapping(address => uint) userDeposit_COMPUTE;
+    mapping(address => uint) public userDeposit;
+    mapping(address => uint) public userEarned;
+    mapping(address => uint) public lastRun;
 
     constructor() {
-        team = msg.sender;
+        owner = msg.sender;
+    }
+    
+    function deposit() public payable {
+        if(msg.sender == owner) {
+            reward = msg.value;
+
+        }
+        else {
+            userDeposit_COMPUTE[msg.sender] += msg.value;
+            userDeposit[msg.sender] += msg.value;
+            poolBalance += msg.value;
+            countDeposit++; 
+            lastRun[msg.sender] = block.timestamp;
+        }
+        require(msg.value > 0, "Can't deposit nothing!");
     }
 
-    modifier onlyTeam() {
-        require(msg.sender == team, "Only the team can call this function.");
-        _;
+    function ifTheAllRewardsAreWithdraw() private {
+        if(countWithdraw == countDeposit)
+            poolBalance = 0;
     }
 
-    function depositRewards() external payable onlyTeam {
-        require(msg.value > 0, "Reward must be greater than 0");
-        require(totalPool > 0, "No deposits to reward");
-
-        // Update the accumulated reward per ETH
-        uint256 rewardPerEth = msg.value.mul(1e12).div(totalPool);
-        accRewardPerEth = accRewardPerEth.add(rewardPerEth);
-        totalPool = totalPool.add(msg.value);
-        rewardCounter++;
-
-        emit RewardAdded(address(this), "Added rewards to pool!");
-    }
-
-    function deposit() external payable {
-        require(msg.value > 0, "Deposit must be greater than 0");
-
-        // Track the user's deposit and their share of the rewards at the moment of deposit
-        userDeposits[msg.sender].push(Deposit({
-            amount: msg.value,
-            rewardEligibilityCounter: rewardCounter // Current counter at the time of deposit
-        }));
-        totalPool = totalPool.add(msg.value);
-
-        emit DepositMade(msg.sender, msg.value);
-    }
-
-
-    function withdraw() external {
-        uint256 totalAmount = 0;
+    function withdraw() public {
         
-        for (uint256 i = 0; i < userDeposits[msg.sender].length; i++) {
-            Deposit memory userDeposit = userDeposits[msg.sender][i];
+        require(msg.sender != owner, "Owner can't withdra!"); 
+        require(countDeposit >= countWithdraw, "All rewards are cleaned!"); 
+        require(userDeposit_COMPUTE[msg.sender] > 0, "You withdraw befor!"); 
 
-            // Calculate total eligible rewards based on deposit timing
-            if (userDeposit.rewardEligibilityCounter < rewardCounter) {
-                uint256 depositReward = userDeposit.amount.mul(accRewardPerEth).div(1e12);
-                totalAmount = totalAmount.add(userDeposit.amount).add(depositReward);
-            } else {
-                // Deposit not eligible for rewards added after the deposit
-                totalAmount = totalAmount.add(userDeposit.amount);
-            }
+        if(block.timestamp - lastRun[msg.sender] > 45 seconds) {
+            uint temp1 = userDeposit_COMPUTE[msg.sender]; 
+            uint temp2 = (((userDeposit_COMPUTE[msg.sender]*100) / poolBalance) * reward) / 100;
+
+            payable(msg.sender).transfer(temp1 + temp2);
+
+            userDeposit_COMPUTE[msg.sender] = 0; 
+            userEarned[msg.sender] += temp2; 
+            countWithdraw++; 
+
+            
+            ifTheAllRewardsAreWithdraw();
+        }
+        else {
+            uint temp1 = userDeposit_COMPUTE[msg.sender]; 
+            uint temp2 = (((userDeposit_COMPUTE[msg.sender]*100) / poolBalance) * reward) / 100;
+            temp2 = temp2 / 2; 
+
+            payable(msg.sender).transfer(temp1 + temp2);
+
+            userDeposit_COMPUTE[msg.sender] = 0; 
+            userEarned[msg.sender] += temp2;
+            countWithdraw++;
+
+            ifTheAllRewardsAreWithdraw();
         }
 
-        require(totalAmount > 0, "User has no balance to withdraw");
-
-        delete userDeposits[msg.sender];
-        totalPool = totalPool.sub(totalAmount).add(totalAmount);
-
-        (bool sent, ) = msg.sender.call{value: totalAmount}("");
-        require(sent, "Failed to send Ether");
-
-        emit Withdrawn(msg.sender, totalAmount);
     }
 
-    function balanceOf(address user) public view returns (uint256) {
-    uint256 totalAmount = 0;
+    function cleanPool() public {
+        require(msg.sender == owner, "Only owner can clean the pool!");
+        require(poolBalance == 0 && address(this).balance > 0);
 
-    for (uint256 i = 0; i < userDeposits[user].length; i++) {
-            Deposit memory userDeposit = userDeposits[user][i];
+        payable(owner).transfer(address(this).balance);
 
-            if (userDeposit.rewardEligibilityCounter < rewardCounter) {
-                uint256 depositReward = userDeposit.amount.mul(accRewardPerEth).div(1e12);
-                totalAmount = totalAmount.add(userDeposit.amount).add(depositReward);
-            } else {
-                totalAmount = totalAmount.add(userDeposit.amount);
-            }
-        }
-
-    // Reflects initial deposit amounts and their earned rewards
-    return totalAmount;
-}
-
-    // Fallback function 
-    fallback() external payable {
-        revert("Please use the deposit function");
     }
-
-    receive() external payable {
-        revert("Please use the deposit function");
-    }
-
-    // Getter 
-    function getPoolValue() public view returns (uint256) {
-        return totalPool;
-    } 
 }
--- test/EthPoolTest.js
+++ test/EthPoolTest.js
@@ -1,5 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+
 
 describe("EthPool", function () {
   let ethPool;
@@ -8,55 +10,79 @@ describe("EthPool", function () {
   let user2;
   let addrs;
 
-  beforeEach(async function () {
+  async function deployPool() {
     // Get signers
-    [owner, user1, user2, ...addrs] = await ethers.getSigners();
 
     // Deploy the EthPool contract
     const EthPool = await ethers.getContractFactory("EthPool");
-    ethPool = await EthPool.deploy();
+    const ethPool = await EthPool.deploy();
+    
+    
+    
+    const [owner] = await ethers.getSigners();
+
+    console.log('Signer 1 address: ', owner.address);
+
     await ethPool.deployed();
-  });
+    return { ethPool, owner };
+  };
+
+  describe("Test Deploy", function () {
+    it('should deploy and set the owner correctly', async function () {
+        const EthPool = await ethers.getContractFactory("EthPool");
+        const ethPool = await EthPool.deploy();
+        const [owner] = await ethers.getSigners();
+        console.log('Signer 1 address: ', owner.address);
+
+        expect(await ethPool.signer.address).to.equal(owner.address);
+      });
+  })
 
 
   describe("Depositing Rewards", function () {
+
     it("Should allow the owner to deposit rewards", async function () {
-        await ethPool.connect(user1).deposit({ value: ethers.utils.parseEther("1.0") });
-        await expect(ethPool.connect(owner).depositRewards({ value: ethers.utils.parseEther("1.0") }))
-            .to.emit(ethPool, "RewardAdded")
-            .withArgs(ethPool.address, "Added rewards to pool!");
+         EthPool = await ethers.getContractFactory("EthPool");
+         ethPool = await EthPool.deploy();
+         const [owner, other] = await ethers.getSigners();
+         console.log('Signer 1 address: ', owner.address);
+ 
+         await expect(ethPool.connect(owner).depositRewards(1))
+        // await expect(ethPool.depositRewards(10))
+        //     .to.emit(ethPool, "RewardAdded")
+        //     .withArgs(ethPool.address, "Added rewards to pool!");
     });
     
 
-    it("Should reject non-owner depositing rewards", async function () {
-        await expect(ethPool.connect(user1).depositRewards({ value: ethers.utils.parseEther("1.0") }))
-            .to.be.revertedWith("Only the team can call this function.");
-        });
+    // it("Should reject non-owner depositing rewards", async function () {
+    //     await expect(ethPool.connect(user1).depositRewards(1))
+    //         .to.be.revertedWith("Only the team can call this function.");
+    //     });
     });
 
 
     describe("Making Deposits", function () {
         it("Should allow users to deposit ETH", async function () {
-            await expect(ethPool.connect(user1).deposit({ value: ethers.utils.parseEther("0.5") }))
+            await expect(ethPool.connect(user1).deposit(100))
                 .to.emit(ethPool, "DepositMade")
-                .withArgs(user1.address, ethers.utils.parseEther("0.5"));
+                .withArgs(user1.address, 100);
         });
 
         it("Should revert when attempting to deposit zero rewards", async function() {
-            await ethPool.connect(user1).deposit({ value: ethers.utils.parseEther("1.0") });
-            await expect(ethPool.connect(owner).depositRewards({ value: 0 }))
+            await ethPool.connect(user1).deposit(100);
+            await expect(ethPool.connect(owner).depositRewards(0))
                 .to.be.revertedWith("Reward must be greater than 0");
         });
         
 
         it("Should correctly handle multiple deposits and withdrawals by the same user", async function() {
-            await ethPool.connect(user1).deposit({ value: ethers.utils.parseEther("0.5") });
-            await ethPool.connect(owner).depositRewards({ value: ethers.utils.parseEther("0.5") });
-            await ethPool.connect(user1).deposit({ value: ethers.utils.parseEther("1.0") });
+            await ethPool.connect(user1).deposit(100);
+            await ethPool.connect(owner).depositRewards(100);
+            await ethPool.connect(user1).deposit(200);
         
 
-            await expect(() => ethPool.connect(user1).withdraw())
-                .to.changeEtherBalance(user1, ethers.utils.parseEther("2.0")); 
+            await expect(() => ethPool.connect(user1).withdraw(100))
+                .to.changeEtherBalance(user1, 200); 
         });        
         
     });
@@ -65,23 +91,23 @@ describe("EthPool", function () {
     describe("Withdrawing", function () {
         it("Should allow users to withdraw their balance including rewards", async function () {
             // User1 deposit
-            await ethPool.connect(user1).deposit({ value: ethers.utils.parseEther("0.5") });
+            await ethPool.connect(user1).deposit(100);
             // Deposit rewards
-            await ethPool.connect(owner).depositRewards({ value: ethers.utils.parseEther("1.0") });
-            
-            // User1 should get 0.5 (their deposit) + 1.0 (all the rewards) = 1.5 ETH
-            await expect(() => ethPool.connect(user1).withdraw())
-                .to.changeEtherBalance(user1, ethers.utils.parseEther("1.5"));
+            await ethPool.connect(owner).depositRewards(100);
+           
+            await expect(() => ethPool.connect(user1).withdraw(200))
+                .to.changeEtherBalance(user1, ethers.utils.parseEther(0));
         });
 
         it("Should distribute multiple rewards correctly", async function () {
-            await ethPool.connect(user1).deposit({ value: ethers.utils.parseEther("2.0") });
-            await ethPool.connect(owner).depositRewards({ value: ethers.utils.parseEther("2.0") });
-            await ethPool.connect(user2).deposit({ value: ethers.utils.parseEther("2.0") });
-            await ethPool.connect(owner).depositRewards({ value: ethers.utils.parseEther("2.0") });
+            await ethPool.connect(user1).deposit(100);
+            await ethPool.connect(owner).depositRewards(100);
+            // user1 can withdraw 200
+            await ethPool.connect(user2).deposit(100);
+            //user2 can only withdraw 100
             
             const user1BalanceBefore = await ethers.provider.getBalance(user1.address);
-            await ethPool.connect(user1).withdraw();
+            await ethPool.connect(user1).withdraw(200);
             const user1BalanceAfter = await ethers.provider.getBalance(user1.address);
             
             // User1 should have more than their initial deposit
@@ -94,20 +120,20 @@ describe("EthPool", function () {
 
         it("Should not allocate previous rewards to deposits made after those rewards were distributed", async function() {
             // Initial deposit to be eligible for rewards.
-            await ethPool.connect(user1).deposit({ value: ethers.utils.parseEther("1.0") });
+            await ethPool.connect(user1).deposit(100);
             
-            await ethPool.connect(owner).depositRewards({ value: ethers.utils.parseEther("1.0") });
+            await ethPool.connect(owner).depositRewards(100);
             
             // User2 makes a deposit not be eligible for reward.
-            await ethPool.connect(user2).deposit({ value: ethers.utils.parseEther("1.0") });
+            await ethPool.connect(user2).deposit(100);
             
             // Withdraw by user1, expecting to receive both the deposit and the rewards.
-            await expect(() => ethPool.connect(user1).withdraw())
-                .to.changeEtherBalance(user1, ethers.utils.parseEther("2.0"));
+            await expect(() => ethPool.connect(user1).withdraw(200))
+                .to.changeEtherBalance(user1, 0);
             
             // Withdraw by user2, expecting to receive only the deposit, since deposit was made after rewards were distributed.
-            await expect(() => ethPool.connect(user2).withdraw())
-                .to.changeEtherBalance(user2, ethers.utils.parseEther("1.0"));
+            await expect(() => ethPool.connect(user2).withdraw(100))
+                .to.changeEtherBalance(user2, 100);
         });
              
         
@@ -117,9 +143,9 @@ describe("EthPool", function () {
   describe("Balance Of", function () {
     it("Should return the correct balance", async function () {
       // User1 deposit
-      await ethPool.connect(user1).deposit({ value: ethers.utils.parseEther("0.5") });
+      await ethPool.connect(user1).deposit(100);
       // Check balance
-      expect(await ethPool.balanceOf(user1.address)).to.equal(ethers.utils.parseEther("0.5"));
+      expect(await ethPool.balanceOf(user1.address)).to.equal(100);
     });
   });
 });
